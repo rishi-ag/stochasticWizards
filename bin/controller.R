f431f016c90b2799ab8835a418203e7a7a9a2ae7
tim.sf <timstf@gmail.com>
2015-02-21 14:50:51 +0100

    reorganized some things

--- bin/controller.R
+++ bin/controller.R
@@ -2,81 +2,84 @@ require(MASS)
 
 
 # ----------------------------------------------------------------------
-# Perfect State Information Controller
+# Perfect State Information Linear Quadratic Controller
 # ----------------------------------------------------------------------
 #' perfect.info.lqr
 #' 
 #' Steer the system towards a target state, optimizing for quadratic loss.
 #'
 #' @param target (numeric matrix) Target state at each iteration.
-#' @param dynamics (function) State transition between iterations.
-#' @param policy (function) Optimal policy.
-#' @param noise.model (function) Function generating random draws from the noise distribution.
-#' @param params (list) Parameters of the cost function, the noise model and the dynamics. Q penalizes deviations form the targets and R penalizes movement.
-#' @return A list with following elements: target, state, controls, noise, loss, params
+#' @param params (list) Deterministic model parameters. Consists of symmetric weighting matrices \code{Q} and \code{R}.
+#' @param noise.model (list) Model from which the noise is drawn. Consists of a function \code{draw} that returns draws and the parameters \code{mean} and \code{var} of the model.
+#' @return A list with following elements: target, state, controls, noise, loss
 #' @export
 #' @import MASS
 #' @examples
-#' # example to be created
+#' # set target trajectory
+#' target <- matrix(c(1, 1, 1, -1, -1, -1, -1, 1, 1, 1),
+#'     nrow = 5,
+#'     ncol = 2,
+#'     byrow = TRUE
+#'     )
+#'
+#' # run
+#' sim <- perfect.info.lqr(
+#'     target,
+#'     list(Q = diag(1, 2, 2), R = diag(0, 2, 2)),
+#'     list(draw = gaussian.noise, mean = rep(0, 2), var = diag(0.01, 2, 2))
+#'     )
 #'
 
-perfect.info.lqr <- function(target, dynamics, policy, noise.model, params) {
+perfect.info.lqr <- function(target, params, noise.model) {
 
-    # setup
+    # math
+    .policy <- function(x, K, Q, R, mean.noise)
+        -solve(R + K) %*% K %*% (x + mean.noise)
+    .dynamics <- function(x, u, w)
+        x + u + w
+    .riccati.eqn <- function(K, Q, R)
+        K -  K %*% solve(K + R) %*% K + Q
+    .loss <- function(X, U, Q, R)
+        sum(apply(X, 1, function(x) x %*% Q %*% x)) + sum(apply(U, 1, function(u) u %*% R %*% u))
+
+    # preprocess
+    niter <- dim(target)[1]
+    dims <- dim(target)[2]
     target <- target
-    state <- matrix(nrow = dim(target)[1], ncol = dim(target)[2])
+    state <- matrix(nrow = niter, ncol = dims)
     state[1,] <- target[1,]
-    controls <- matrix(nrow = dim(target)[1] - 1, ncol = dim(target)[2])
-    noise <- noise.model(dim(target)[1] - 1, params$mean.noise, params$var.noise)
-
-    # compute K-matrices
-    K <- list(params$Q)
-    for (i in (2:dim(target)[1])) {
-        K[[i]] <- riccati.eqn(K[[i-1]], params$Q, params$R)
-    }
-
+    controls <- matrix(nrow = niter - 1, ncol = dims)
+    noise <- noise.model$draw(niter - 1, noise.model$mean, noise.model$var)
+    params$K <- list(params$Q)
+    for (i in (2:niter))
+        params$K[[i]] <- .riccati.eqn(params$K[[i-1]], params$Q, params$R)
+    
     # run simulation
-    for (i in 1:(dim(state)[1] - 1)) {
+    for (i in 1:(niter - 1)) {
 
         # compute optimal step
-        controls[i,] <- policy(
+        controls[i,] <- .policy(
             state[i,] - target[i + 1,],
-            K[[dim(target)[1] - i]],
+            params$K[[niter - i]],
             params$Q,
             params$R,
-            params$mean.noise
+            noise.model$mean
             )
 
-        # simulate next ioint
-        state[i + 1,] <- dynamics(
-            state[i,],
-            controls[i,],
-            noise[i,]
-            )
+        # simulate next waypoint
+        state[i + 1,] <- .dynamics(state[i,], controls[i,], noise[i,])
     }
 
-    # compute losses
-    dev.loss <- sum(apply(target - state, 1, function(x) x %*% params$Q %*% x))
-    ctrl.loss <- sum(apply(controls, 1, function(u) u %*% params$R %*% u))
+    # compute loss
+    loss <- .loss(target - state, controls, params$Q, params$R)
 
     return(list(
         target = target,
         state = state,
         controls = controls,
         noise = noise,
-        loss = dev.loss + ctrl.loss,
-        params = params
+        loss = loss
         ))
-}
-
-
-policy <- function(state, K, Q, R, mean.noise) {
-    return(-solve(R + K) %*% K %*% (state + mean.noise))
-}
-
-
-dynamics <- function(state, control, noise) {
-    return(state + control + noise)
 }
 
 
@@ -85,9 +88,7 @@ gaussian.noise <- function(n, mean, var) {
 }
 
 
-riccati.eqn <- function(K, Q, R) {
-    return(K -  K %*% solve(K + R) %*% K + Q)
-}
+
 
 
 # TESTING CODE
@@ -100,8 +101,6 @@ target <- matrix(
 
 sim <- perfect.info.lqr(
     target,
-    dynamics,
-    policy,
-    gaussian.noise,
-    list(Q = diag(1, 2, 2), R = diag(0, 2, 2), mean.noise = rep(0, 2), var.noise = diag(0.01, 2, 2))
+    list(Q = diag(1, 2, 2), R = diag(0, 2, 2)),
+    list(draw = gaussian.noise, mean = rep(0, 2), var = diag(0.01, 2, 2))
     )
