3f812956b1c36ed7ab461e06b5eab51ce748db7f
j-verdu <januoc@gmail.com>
2015-03-07 19:08:19 +0100

    small bug at ny.wind.model function

--- lib/stochastics.R
+++ lib/stochastics.R
@@ -14,7 +14,7 @@ ny.wind.model <- function(n) {
     cov <- rbind(cbind(read.csv("data/cov_wind_residuals.csv", row.names = 1), 0), 0) # to build the gaussian shocks
     coefs <- t(rbind(cbind(read.csv("data/coefs_AR1_wind.csv", row.names = 1), 0), 0)) # coefs of the AR1 model
     draws <- ar1.noise(n, coefs, cov)
-    means <- t(cbind(coefs[,1], sapply(2:n, function(i) coefs %*% c(1, noise[i,]))))
+    means <- t(cbind(coefs[,1], sapply(2:n, function(i) coefs %*% c(1, draws[i,]))))
     return(list(draws = draws, means = means))
 }
 
