94eedd6ad421a3c4e4096699f12fd35e14f6e1b8
tim.sf <timstf@gmail.com>
2015-03-16 23:56:00 +0100

    reverting false uploads

--- lib/stochastics.R
+++ lib/stochastics.R
@@ -78,30 +78,6 @@ ny.wind.model <- function(n, wind.ini = c(0,0,0), type = "simulated") {
 }
 
 
-#Calculates GPS uncertainity cov matrix and writes it to data/covariance
-get.GPS.cov <- function() {
-  
-  set.seed(1000)
-  position <- data.frame(X = rnorm(1000000, mean = 10, sd = 1), 
-                         Y = rnorm(1000000, mean = 10, sd = 1), 
-                         Z = rnorm(1000000, mean = 20, sd = 2))
-  sigma <- cov(as.matrix(position))
-  write.table(x = sigma, file = "Covariance")
-}
-
-get.gps.noise <- function(type) {
-  #GPS uncertainity is persistant. Only case to be added is when we dont want it
-  #altogether
-  
-  sigma <- as.matrix(read.table("Covariance", header = T))
-  coord.noise <- c(0,0,0)
-  if(type != "no_GPS"){
-    coord.noise <- mvtnorm::rmvnorm(n = 1, mean = c(10,10,20), sigma = sigma)
-    
-  }
-  return(coord.noise)
-}
-
 get.gps.noise <- function(noShocks) {
   sigma <- as.matrix(read.table("data/Covariance", header = T))
   mvrnorm(n = noShocks, mu = c(0,0,0), Sigma = sigma)
