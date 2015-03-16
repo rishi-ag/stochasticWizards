b3e49f325cd9bfa2ef8a0b7ea2dc9ef8fc13d9a7
rishi1226 <agnihotri.rishabh@gmail.com>
2015-03-16 19:56:11 +0100

    Kalman Filter

---
+++ lib/kalman.R
@@ -0,0 +1,32 @@
+library("assertthat")
+
+kalman.filter <- function(est.state.prev, control, noisy.state, Q, R, P.prev) {
+  assert_that(is.matrix(est.state.prev), nrow(est.state.prev) == 3, 
+              ncol(est.state.prev) == 1)
+  assert_that(is.matrix(control), nrow(control) == 3, ncol(control) == 1)
+  assert_that(is.matrix(noisy.state), nrow(noisy.state) == 3, 
+              ncol(noisy.state) == 1)
+  assert_that(is.matrix(P.prev), nrow(P.prev) ==3,  ncol(P.prev) == 3)
+  assert_that(is.matrix(Q), nrow(Q) ==3,  ncol(Q) == 3)
+  assert_that(is.matrix(R), nrow(R) ==3,  ncol(R) == 3)
+  
+  #Predict Stage
+  #Predicted (a priori) state estimate
+  est.state <- est.state.prev + control
+  #Predicted (a priori) estimate covariance
+  P.new <- P.prev + Q
+  
+  #Update Stage
+  #Innovation or measurement residual
+  y <- noisy.state - est.state
+  #Innovation (or residual) covariance
+  S <- P.new 
+  #Optimal Kalman gain
+  K <- P.new %*% solve(S)
+  #Updated (a posteriori) state estimate
+  est.state <- est.state + K %*% y
+  #Updated (a posteriori) estimate covariance
+  P.new <- (diag(3) - K) %*% P.new
+  
+  return(list(est.state = est.state, P.new = P.new))
+}
